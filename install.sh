94e3cd32683d8a32f9584db107de9c602a33e38d
fankuan <fmkuan44@gmail.com>
2023-10-31 14:33:29 +0700

    update README.md

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
-pip install -r requirement.txt
+pip install -r requirements.txt
 
-gdown https://drive.google.com/uc?id=1Ud4GF-3264R0rkqOmBK46L9S_kalN01z -O database/nb201/[cifar10]_data.p
-gdown https://drive.google.com/uc?id=1mow6Cqwgs3DnKngF6reF7STsVa7KS15R -O database/nb201/[cifar100]_data.p
-gdown https://drive.google.com/uc?id=1LSqvNkFzsRldW2kgCaSao9uCdYpN525H -O database/nb201/[ImageNet16-120]_data.p
-gdown https://drive.google.com/uc?id=1Zbb_StAGgX6HYHHuoZWPY8PLb0DhDzKY -O database/nb201/zc_database.json
+gdown https://drive.google.com/uc?id=1Ud4GF-3264R0rkqOmBK46L9S_kalN01z -O database/NASBench201/[CIFAR-10]_data.p
+gdown https://drive.google.com/uc?id=1mow6Cqwgs3DnKngF6reF7STsVa7KS15R -O database/NASBench201/[CIFAR-100]_data.p
+gdown https://drive.google.com/uc?id=1LSqvNkFzsRldW2kgCaSao9uCdYpN525H -O database/NASBench201/[ImageNet16-120]_data.p
+gdown https://drive.google.com/uc?id=1Zbb_StAGgX6HYHHuoZWPY8PLb0DhDzKY -O database/NASBench201/zc_nasbench201.json
+gdown https://drive.google.com/uc?id=1DRRnPJOvKwOE1g4n9TuoOP6bQ5KjGnwl -O database/NASBench201/logsynflow_nwot.p
 
-gdown https://drive.google.com/uc?id=1y9pxpnYkhOsg-3xeNwhJYg9gVtOwipHT -O database/nb101/data.p
-gdown https://drive.google.com/uc?id=1V5Nz9SlMvwYSnQq1-1bJNvob8i1o4HiK -O database/nb101/zc_database.p
-gdown https://drive.google.com/uc?id=1--RQXQU1r6UZOVF9kI2IKaofETOkcTRG -O database/nb101/flops_database.p
+gdown https://drive.google.com/uc?id=1y9pxpnYkhOsg-3xeNwhJYg9gVtOwipHT -O database/NASBench101/data.p
+gdown https://drive.google.com/uc?id=1V5Nz9SlMvwYSnQq1-1bJNvob8i1o4HiK -O database/NASBench101/zc_101.p
+gdown https://drive.google.com/uc?id=1--RQXQU1r6UZOVF9kI2IKaofETOkcTRG -O database/NASBench101/flops_all_arch_101.p
+gdown https://drive.google.com/uc?id=1-6M0m6OkuAFfwOQ6k3HXLmrZtseLugs0 -O database/NASBench101/logsynflow_nwot_skip.p
